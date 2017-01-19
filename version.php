5c649cabce64d2d3bd253ac21f20e8921d5cfe54
Sixto Martin <pitbulk@gmail.com>
2017-01-19 05:15:09 +0100

    New release

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
-$plugin->version = 2017111801;
+$plugin->version = 2017011901;
 $plugin->requires = 2012062500;
-$plugin->release = 'auth/saml version of 18-11-2016';
+$plugin->release = 'auth/saml version of 19-01-2017';
 $plugin->maturity = MATURITY_BETA;
 $plugin->component = 'auth_saml';
