1617855efa64518a32845adaa7f17e97c8bdfcdf
Sixto Martin <pitbulk@gmail.com>
2019-01-15 19:47:57 +0100

    Mention that 3.6 is supported. Fix plugin->requires

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
 $plugin->version = 2019011501;
-$plugin->requires = 2018090300;
+$plugin->requires = 2017111300;
 $plugin->release = 'auth/saml version of 15-01-2019';
 $plugin->maturity = MATURITY_BETA;
 $plugin->cron = 0;
